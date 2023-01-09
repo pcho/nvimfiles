cd3693921d8c470a18450426a59add3a1a983fc7
pcho <p@monkin.co>
2023-01-09 12:38:43 +0100

    Up

--- lua/pcho/plugins/treesitter.lua
+++ lua/pcho/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'prisma', 'lua', 'json', 'javascript', 'tsx', 'toml', 'yaml', 'typescript', 'help', 'vim', 'bash', 'html', 'css', 'bash' },
+    ensure_installed = { 'prisma', 'lua', 'json', 'javascript', 'tsx', 'toml', 'yaml', 'typescript', 'help', 'vim', 'bash', 'html', 'css', 'go', 'python', 'rust', },
     autotag = {
         enable = true,
         filetypes = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'tsx', 'jsx', },
