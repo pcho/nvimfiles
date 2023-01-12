ac9d31b9e3a3abdbf34afa927af39fed13c5c1a5
pcho <p@monkin.co>
2023-01-12 07:50:23 +0100

    Move back to one file

--- lua/pcho/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'prisma', 'lua', 'json', 'javascript', 'tsx', 'toml', 'yaml', 'typescript', 'help', 'vim', 'bash', 'html', 'css', 'go', 'python', 'rust', },
-    autotag = {
-        enable = true,
-        filetypes = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'tsx', 'jsx', },
-    },
-    highlight = { enable = true },
-    indent = { enable = true },
-}
