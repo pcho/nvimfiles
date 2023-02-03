a338a0e15a1b0505022358b3e45b95485cf732c6
pcho <p@monkin.co>
2023-02-03 15:50:00 +0100

    Up

--- lua/pcho/plugins/fugitive.lua
+++ lua/pcho/plugins/fugitive.lua
@@ -1,1 +1,4 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gm', ':Git push -u origin master<cr>');
+vim.keymap.set('n', '<leader>gp', ':Git push -u origin ');
+vim.keymap.set('n', '<leader>gr', ':Git reset HEAD~1 --soft<cr>');
